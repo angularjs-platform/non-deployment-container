aec34e02a4b51432178f284839801a7f5626fc02
Bipin Poudyal <bipin.poudyal@misys.com>
2016-07-28 12:32:44 +0530

    intial organization changes

---
+++ src/internal-libs.ts
@@ -0,0 +1,10 @@
+// Theme
+import '@norn/non-theme-base';
+import '@norn/non-theme-default';
+
+// Core
+import '@norn/non-framework';
+
+// IFU
+import '@norn/non-ifu-login';
+import '@norn/non-ifu-organization';
