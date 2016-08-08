a94bb285e85b7e8d5f18a8a1a19f41e001901276
Superman <superman08998@gmail.com>
2016-08-08 09:54:16 +0530

    feat(layout-navigation): Layout and Navigation Architecture

--- src/container.module.ts
+++ src/container.module.ts
@@ -3,7 +3,10 @@ const moduleName: string = 'non.container';
 // SCSS
 import './container.scss';
 
+// Favicon
+import './favicon.ico';
+
 // Internal modules
 import app from './app/app.module';
 
-angular.module(moduleName, ['non.framework', 'non.theme-default', 'non.theme-base', 'non.ifu.login', 'non.ifu.organization', app]);
+angular.module(moduleName, ['non.framework', 'non.theme-base', 'non.ifu.main', 'non.ifu.home', 'non.ifu.login', 'non.ifu.organization', app]);
