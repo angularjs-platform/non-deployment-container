5d71ad39c8e9e6b1ceba6ad523a6be6bd3b2456f
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2016-08-08 09:54:16 +0530

    feat(layout-navigation): Layout and Navigation Architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,10 @@
-import {IConfigurationService} from '@norn/non-framework';
-
-const template: string = require('./main.tpl');
-
 export class UIRouterConfig {
 
     constructor(
-        private $stateProvider: ng.ui.IStateProvider,
         private $urlRouterProvider: ng.ui.IUrlRouterProvider) {
         'ngInject';
 
         this.$urlRouterProvider.otherwise('/login');
-
-        this.$stateProvider
-            .state('app', {
-                template: template,
-                controller: 'MainController',
-                controllerAs: 'mainCtrl',
-                abstract: true,
-                resolve: {
-                    config: this.loadConfiguration
-                }
-            });
     }
-
-    private loadConfiguration = (ConfigurationService: IConfigurationService): any => {
-        'ngInject';
-        return ConfigurationService.loadConfig();
-    };
 }
 
--- src/vendor-libs.ts
+++ src/vendor-libs.ts
@@ -5,3 +5,6 @@ import 'angular-ui-router';
 import 'angular-aria';
 import 'angular-animate';
 import 'angular-material';
+import 'angular-loading-bar';
+import 'angular-cookies';
+import 'angular-material-sidemenu';
