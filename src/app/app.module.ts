0c06161ea03d8b63da3ad90266dbc82616cf6878
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2016-10-21 19:41:25 +0530

    feat(localstorage): introduce local storage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {HTTPConfig} from './app.http.config';
 import {Run} from './app.run';
 import {LocalizationConfig} from './app.localization.config';
 import {LoadingBarConfig} from './app.loading-bar.config';
+import {LocalStorageConfig} from './app.localstorage.config';
 
 import {AppController} from './app.controller';
 
@@ -14,6 +15,7 @@ angular.module(moduleName, [])
     .config(UIRouterConfig)
     .config(HTTPConfig)
     .config(LocalizationConfig)
-    .config(LoadingBarConfig);
+    .config(LoadingBarConfig)
+    .config(LocalStorageConfig);
 
 export default moduleName;
