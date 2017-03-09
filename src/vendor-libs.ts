00d6f59589230caabf22459b734778e1a7030e05
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2017-03-09 18:52:34 +0530

    feat(Localization): Namespacing Localization

--- src/vendor-libs.ts
+++ src/vendor-libs.ts
@@ -15,7 +15,6 @@ import 'angular-translate';
 import 'angular-translate-loader-partial';
 import 'angular-translate-storage-cookie';
 import 'angular-translate-storage-local';
-import 'angular-translate-handler-log';
 import 'angular-local-storage';
 import 'angular-ui-grid';
 import 'angular-breadcrumb';
